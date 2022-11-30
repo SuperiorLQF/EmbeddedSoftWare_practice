0a051dcaf91a02b0a1e225bea5d90ceb88cd685b
superiorlqf <superiorlqf@gmail.com>
2022-11-30 16:35:58 +0800

    创建CMake

---
+++ lab4/build/CMakeFiles/readfirst.o.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/readfirst.o.dir/readfirst.c.o"
+  "CMakeFiles/readfirst.o.dir/readfirst.c.o.d"
+  "readfirst.o"
+  "readfirst.o.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/readfirst.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab4/build/CMakeFiles/readfirst.o.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for readfirst.o.
+# This may be replaced when dependencies are built.
